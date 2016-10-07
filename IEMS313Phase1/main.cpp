38ccbff0de63561a9cf218066bb6c3733e796c87
Will Finnegan <finnegan.wl@gmail.com>
2016-10-07 15:21:46 -0500

    Initial Commit

---
+++ IEMS313Phase1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IEMS313Phase1
+//
+//  Created by Will Finnegan on 10/7/16.
+//  Copyright © 2016 Will Finnegan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
